a74c51d2321abd395baf649e2ff36ac4f497b66e
unknown <anassaleem555590@gmail.com>
2024-03-14 08:02:14 +0500

    first-commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display")
+
+function AppendToDisplay (input){
+    display.value += input;
+}
+
+function cleardisplay(){
+    display.value = ""
+}
+function calculate (){
+    try{display.value = eval(display.value)
+    }
+    catch(Error){
+        display.value = 'Error'
+    } 
+
+    
+}
